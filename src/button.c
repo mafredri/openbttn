d5436f0b5372f5d66e8291c2418da9004a049aff
Mathias Fredriksson <mafredri@gmail.com>
2016-10-30 20:52:32 +0200

    Proper button pressed / duration implementation

--- src/button.c
+++ src/button.c
@@ -2,11 +2,9 @@
 
 #include "button.h"
 
-volatile bool g_buttonPressed = false;
+ButtonState g_button;
 
 void button_Init(void) {
-  g_buttonPressed = false;
-
   rcc_periph_clock_enable(RCC_GPIOC);
   rcc_periph_clock_enable(RCC_SYSCFG);
 
@@ -19,16 +17,38 @@ void button_Init(void) {
   exti_set_trigger(EXTI13, EXTI_TRIGGER_RISING);
   exti_enable_request(EXTI13);
 
-  nvic_enable_irq(NVIC_EXTI15_10_IRQ);
   nvic_set_priority(NVIC_EXTI15_10_IRQ, (4 << 4));
+  nvic_enable_irq(NVIC_EXTI15_10_IRQ);
+
+  button_Reset();
+  g_button.pressed = gpio_get(BUTTON_GPIO_PORT, BUTTON_PIN_IT);
 }
 
-// TODO: Not system tick.
-uint32_t button_PressedDuration(void) { return g_SystemTick; }
+void button_Reset(void) {
+  g_button.pressed = false;
+  g_button.duration = 0;
+}
+
+bool button_IsPressed(void) { return g_button.pressed; }
+
+uint32_t button_PressedDuration(void) { return g_button.duration; }
+
+void button_SysTickHandler(void) {
+  if (g_button.pressed) {
+    if (gpio_get(BUTTON_GPIO_PORT, BUTTON_PIN_IT)) {
+      g_button.duration++;
+    } else {
+      g_button.pressed = false;
+    }
+  }
+}
 
 void BUTTON_ISR(void) {
   if (exti_get_flag_status(EXTI13)) {
+    if (gpio_get(BUTTON_GPIO_PORT, BUTTON_PIN_IT)) {
+      g_button.pressed = true;
+      g_button.duration = 0;
+    }
     exti_reset_request(EXTI13);
-    g_buttonPressed = true;
   }
 }
--- src/button.h
+++ src/button.h
@@ -10,9 +10,16 @@
 #define BUTTON_ISR exti15_10_isr
 #define BUTTON_NVIC NVIC_EXTI15_10_IRQ
 
-extern volatile bool g_buttonPressed;
+typedef struct ButtonState ButtonState;
+struct ButtonState {
+  volatile bool pressed;
+  volatile uint16_t duration;
+};
 
 void button_Init(void);
+void button_Reset(void);
+bool button_IsPressed(void);
 uint32_t button_PressedDuration(void);
+void button_SysTickHandler(void);
 
 #endif /* BUTTON_H */
