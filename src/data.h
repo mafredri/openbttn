92993141805989ac2adecccaf1b47def1da92b17
Mathias Fredriksson <mafredri@gmail.com>
2016-10-30 21:11:23 +0200

    Update data (index.html, firstset.html)

--- src/data.h
+++ src/data.h
@@ -3,10 +3,10 @@
 
 #include <stdint.h>
 
-#define DATA_FIRSTSET_HTML_LENGTH 3509
-#define DATA_INDEX_HTML_LENGTH 2439
+#define DATA_FIRSTSET_HTML_GZ_LENGTH 2169
+#define DATA_INDEX_HTML_GZ_LENGTH 1390
 
-extern uint8_t g_DataIndexHtml[];
-extern uint8_t g_DataFirstsetHtml[];
+extern uint8_t g_DataIndexHtmlGz[];
+extern uint8_t g_DataFirstsetHtmlGz[];
 
 #endif /* DATA_H */
