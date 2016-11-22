5ecf7ec8fd3341b5d77299eac4159c630b6d247c
Mathias Fredriksson <mafredri@gmail.com>
2016-11-22 18:58:58 +0200

    Use constants for data

--- src/data.h
+++ src/data.h
@@ -6,7 +6,7 @@
 #define DATA_FIRSTSET_HTML_GZ_LENGTH 2201
 #define DATA_INDEX_HTML_GZ_LENGTH 1486
 
-extern uint8_t g_DataIndexHtmlGz[];
-extern uint8_t g_DataFirstsetHtmlGz[];
+extern const uint8_t g_DataIndexHtmlGz[];
+extern const uint8_t g_DataFirstsetHtmlGz[];
 
 #endif /* DATA_H */
