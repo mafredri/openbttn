5763dfd912fc6c4b40671bef1ba6e513fa779d4a
Mathias Fredriksson <mafredri@gmail.com>
2017-06-23 01:13:27 +0300

    Fix firmware update link in firstset.html

--- src/data.h
+++ src/data.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-#define DATA_FIRSTSET_HTML_GZ_LENGTH 2340
+#define DATA_FIRSTSET_HTML_GZ_LENGTH 2342
 #define DATA_INDEX_HTML_GZ_LENGTH 1494
 
 extern const uint8_t g_DataIndexHtmlGz[];
