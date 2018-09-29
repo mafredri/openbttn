13c8729f0e52a4a5b77877a345898cc5e1714560
Mathias Fredriksson <mafredri@gmail.com>
2018-09-29 03:32:18 +0300

    Generate updated data.{c,h}

--- src/data.h
+++ src/data.h
@@ -3,10 +3,10 @@
 
 #include <stdint.h>
 
-#define DATA_FIRSTSET_HTML_GZ_LENGTH 2418
+#define DATA_FIRSTSET_HTML_GZ_LENGTH 2416
 #define DATA_INDEX_HTML_GZ_LENGTH 1549
 
-extern const uint8_t g_DataIndexHtmlGz[];
 extern const uint8_t g_DataFirstsetHtmlGz[];
+extern const uint8_t g_DataIndexHtmlGz[];
 
 #endif /* DATA_H */
